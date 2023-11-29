44cb4f41944de14a37c971c76d56c8b5b1510e68
Admin <Admin@dell>
2023-11-29 16:18:31 +0530

    Learning

---
+++ Learning_java/src/Learning_java.java
@@ -0,0 +1,13 @@
+
+public class Learning_java {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		
+		add(a,b);
+	}
+	public static void add(int a,int b){
+		int c=a+b;
+		System.out.println("Addition of two values "+c);
+	}
+}
